f26987bcc2caaa9e1563e284668087fa7081a12d
Nestor <nestor.lenaerts@gmail.com>
2024-03-19 19:25:48 +0100

    update van index

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-let currentPage = 1;
-/*GALERY GEDEELTE */
-function updateGallery() {
-    document.querySelectorAll('.image-gallery').forEach(gallery => {
-        gallery.classList.remove('visible');
-    });
-    document.getElementById(`gallery${currentPage}`).classList.add('visible');
-    
-    document.getElementById('prevBtn').disabled = currentPage === 1;
-    document.getElementById('nextBtn').disabled = currentPage === 3; 
-
-    document.querySelectorAll('.page-number').forEach(page => {
-        page.classList.remove('active');
-    });
-    document.getElementById(`page${currentPage}`).classList.add('active');
-}
-
-document.getElementById('nextBtn').addEventListener('click', function() {
-    if (currentPage < 3) {
-        currentPage++;
-        updateGallery();
-    }
-});
-
-document.getElementById('prevBtn').addEventListener('click', function() {
-    if (currentPage > 1) {
-        currentPage--;
-        updateGallery();
-    }
-});
-
-document.querySelectorAll('.page-number').forEach(page => {
-    page.addEventListener('click', function() {
-        currentPage = parseInt(this.textContent);
-        updateGallery();
-    });
-});
-
 
 
 /*MODAL GEDEELTE VAN MAIN PAGE*/
@@ -79,21 +41,3 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   }
 });
-
-
-function addDeck() {
-  const decksContainer = document.getElementById('decks-container');
-  console.log("GEDRUKT OP DE ADD DECKS")
-  if (decksContainer.children.length >= 8) {
-      alert("Maximum of 8 decks reached.");
-      return;
-  }
-
-  const template = document.getElementById('deckTemplate');
-  const clone = document.importNode(template.content, true);
-  
-  const deckContainer = clone.querySelector('.deck-container');
-  deckContainer.onclick = function() { window.location.href = 'deckdetailpage.html'; }; //DIT VERANDEREN OM NAAR DE DECKDETAILPAGE OF ANDERE TE GAAN !!
-
-  decksContainer.appendChild(clone);
-}

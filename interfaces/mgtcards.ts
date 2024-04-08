42eb9dc04ab5925ea8a7e96f306bb5276e502170
Nestor <nestor.lenaerts@gmail.com>
2024-04-08 18:00:11 +0000

    backend testen

---
+++ interfaces/mgtcards.ts
@@ -0,0 +1,19 @@
+export interface Card {
+    name: string;
+    names: string[];
+    cmc: number;
+    colors: string[];
+    type: string;
+    rarity: string;
+    text: string;
+    artist: string;
+    number: string;
+    power: string;
+    toughness: string;
+    imageUrl: string;
+  }
+  
+
+export interface CardsResponse {
+    cards: Card[];
+  }
--- src/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const header = document.querySelector("header");
-
-menuBtn.addEventListener("click", () => {
-  header.classList.toggle("flex");
-  header.classList.toggle("hidden");
-});

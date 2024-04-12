e8918447dece0f32894901ea3f888bbd408a9778
Btiisseem <ibtissambouchouari@gmail.com>
2024-04-12 15:27:54 +0000

    appjs

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const path = require('path');
 
 const app = express();
 
-// Set up view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Serve static files from the "public" directory (als je een 'public' map hebt)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Definieer routes voor elke pagina
+
 app.get('/', (req, res) => {
-  res.render('landingpage'); // De ejs-file voor je landing page
+  res.render('landingpage'); 
 });
 
 app.get('/login', (req, res) => {
@@ -47,15 +45,9 @@ app.get('/detailpage', (req, res) => {
   res.render('detailpage');
 });
 
-// Zorg ervoor dat je voor elke unieke route een unieke view hebt.
-// Als er meer pagina's zijn, ga door met het definiëren van routes op een vergelijkbare manier.
 
-// Als je een 404 pagina hebt:
-app.use((req, res, next) => {
-  res.status(404).render('404'); // Zorg ervoor dat je een 404.ejs bestand hebt in je views map
-});
 
-// Start de server
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

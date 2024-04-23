12239f1ce0ec76e67a53f7a38af206ae6b70de79
Nestor <nestor.lenaerts@gmail.com>
2024-04-23 17:07:41 +0000

    pagiantie bij main in orde gebracht

--- routes/index.ts
+++ routes/index.ts
@@ -16,27 +16,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //const api 
-
+const cardsPerPage = 9;
 
 app.get("/", async (req, res) => {
-
-
     res.render("index")
 })
 
 app.get("/main", async (req, res) => {
-    const page = req.query.p || 1; 
-    const cardsPerPage = 9; 
-    let cards: Card[] = await getPageCard((Number(page) * cardsPerPage));
+    const page = parseInt(req.query.p as string) || 1;
+
+    const totalCards = (await getAllCards()).length;
+    const totalPages = Math.ceil(totalCards / cardsPerPage);
+    let cards: Card[] = await getPageCard(page);
     res.render("main", {
-        cards : cards
+        cards : cards,
+        currentPage: page,
+        totalPages: totalPages,
     })
 })
 
 
+app.get('/next', async (req, res) => {
+    const page = parseInt(req.query.p as string) || 1;
+    const nextPage = page + 1;
+    const totalCards = (await getAllCards()).length;
+    const cards = await getPageCard(nextPage);
+    const totalPages = Math.ceil(totalCards / cardsPerPage);
+    res.render('main', {
+      cards,
+      currentPage: nextPage,
+      totalPages: totalPages,
+    });
+  });
 
 
+  app.get('/register' , (req, res)=>{
 
+    res.render('register')
+  })
+  app.get('/overview' , (req, res)=>{
+
+    res.render('overview')
+  })
 
 app.listen(app.get("port"), async () => {
     await connect();

ca89a0fef6b75215b40f94a28ab92aa2ec68345d
kubra kizililic <kubra.kizilkilic@student.ap.be>
2024-05-08 15:29:07 +0000

    hamburger menu aangemaakt

--- public/js/styling-register.js
+++ public/js/styling-register.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
     // Get the value of userEmailExists
     const userEmailExists = <%= userEmailExists %>;
     const emailInput = document.getElementById('email');
@@ -7,5 +7,20 @@ window.onload = function() {
       // Update the class based on the value of userEmailExists
       emailInput.classList.add('error');
     }
-};
+};*/
   
+
+// Hamburger menu
+document.addEventListener('DOMContentLoaded', function () {
+  var hamburger = document.getElementById('hamburger');
+  var navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', function () {
+      // Check if the menu is open
+      if (navMenu.style.width == '350px') {
+          navMenu.style.width = '0';
+      } else {
+          navMenu.style.width = '350px';
+      }
+  });
+});
